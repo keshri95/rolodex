4b25f28b604e70947c186cdbd0e4b1ce31d4adef
Ashish Keshri <keshri.ashish1@gmail.com>
2023-02-03 20:36:14 +0530

    fetch-json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import Home from "./components/Home";
 
-function App() {
+const App = () => {
+  const [monsters, setMonsters] = useState([]);
+  const API = `https://jsonplaceholder.typicode.com/users`;
+
+  const Rolodex = async (url) => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMonsters(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    Rolodex(API);
+  }, []);
+
   return (
-   <p>rolodex</p>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography>Rolodex</Typography>
+        </Toolbar>
+      </AppBar>
+      <Home monsters={monsters} />
+    </div>
   );
-}
+};
 
 export default App;
