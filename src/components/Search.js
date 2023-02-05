5948c51d37289fdcbd476380e2f12512e2fa6100
Ashish Keshri <keshri.ashish1@gmail.com>
2023-02-05 10:41:29 +0530

    search

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Grid, Paper } from "@mui/material";
+
+const Search = ({monsters, onChangeHandler }) => {
+  
+
+  return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Paper component='form' sx={{ p: 2, boxShadow: 'none', m: 2}}>
+        <input type="search" placeholder="Search here" onChange={onChangeHandler} />
+      </Paper>
+    </Grid>
+  );
+};
+
+export default Search;
