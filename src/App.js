0c178264d7c2afc2ac00d6a9d87bd779f3bcd069
Ashish Keshri <keshri.ashish1@gmail.com>
2023-02-06 22:06:06 +0530

    dark-mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import Home from "./components/Home";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Container
+} from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import AdbIcon from "@mui/icons-material/Adb";
+
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { Suspense, useEffect, useState } from "react";
+import React, { lazy } from "react";
+const Home = React.lazy(() => import("./components/Home"));
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
 
 const App = () => {
   const [monsters, setMonsters] = useState([]);
+  const [theme, setTheme] = useState('light-mode');
   const API = `https://jsonplaceholder.typicode.com/users`;
 
   const Rolodex = async (url) => {
@@ -20,14 +41,37 @@ const App = () => {
     Rolodex(API);
   }, []);
 
+    const changeTheme = () => {
+    
+      if(theme === "dark-mode"){
+          setTheme("light-mode");
+      } else{
+          setTheme('dark-mode');
+      }
+    }
+
+    useEffect(() => {
+      document.body.className = theme;
+    }, [theme])
+
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography>Rolodex</Typography>
-        </Toolbar>
-      </AppBar>
-      <Home monsters={monsters} />
+      <ThemeProvider theme={darkTheme}>
+        <AppBar position="static">
+          <Container maxWidth="xl">
+          <Toolbar>
+            <AdbIcon />
+            <Typography variant="h5" sx={{ flexGrow: 1,display: { xs: "block", sm: 'block' }}}>Rolodex</Typography>
+              <IconButton size="large" onClick={changeTheme}>
+                <DarkModeIcon />
+              </IconButton>              
+          </Toolbar>
+          </Container>
+        </AppBar>
+      </ThemeProvider>
+      <Suspense fallback={<div>Please wait...</div>}>
+          <Home monsters={monsters} />
+      </Suspense>
     </div>
   );
 };
