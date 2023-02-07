37f41dda8a8ca409fdaf41fdca4f5d5981a05ed2
Ashish Keshri <keshri.ashish1@gmail.com>
2023-02-07 19:28:29 +0530

    home

---
+++ src/components/Home.js
@@ -0,0 +1,48 @@
+import { Box, Stack } from "@mui/system";
+import React, { useEffect, useState } from "react";
+const Filter = React.lazy(() => import("./Filter"));
+const CardContainer = React.lazy(() => import("./CardContainer"));
+const Search = React.lazy(() => import("./Search"));
+const Home = ({ monsters }) => {
+
+  const [search, setSearch] = useState('');
+  const [filteredRolo, setFilteredRolo] = useState(monsters);
+
+  const onChangeHandler = (e)=> {
+    const searchRolo = e.target.value.toLocaleLowerCase();
+    setSearch(searchRolo);
+  }
+
+  useEffect(() => {
+
+    const searchedRolo = monsters.filter((item) =>{
+      return item.name.toLocaleLowerCase().includes(search)
+    })
+
+    setFilteredRolo(searchedRolo);
+
+  }, [search, monsters]);
+
+
+
+
+
+  return (
+    <Stack
+      sx={{ flexDirection: { sx: "column", md: "row" } }}
+      direction="row"
+      spacing={2}
+    >
+
+
+    
+
+      <Box>
+          <Search monsters={monsters} onChangeHandler={onChangeHandler} />
+          <CardContainer monsters={monsters} filteredRolo={filteredRolo} setFilteredRolo={setFilteredRolo}  />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Home;
