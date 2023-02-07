215e9384d66726effc1f092b0df55cbb1db37e36
Ashish Keshri <keshri.ashish1@gmail.com>
2023-02-07 19:10:07 +0530

    searching

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid, Paper } from "@mui/material";
 
-const Search = ({monsters, onChangeHandler }) => {
+const Search = ({ onChangeHandler }) => {
   
 
   return (
